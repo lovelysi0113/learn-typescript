14797b0a58010c608702096e3cc46f08dff0dc43
Jung SongYi <lovelysi0113@gmail.com>
2021-04-17 17:33:10 +0900

    4-첫 번째 프로젝트 - 할 일 관리 애플리케이션 (1)

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,9 @@
-let todoItems;
+// 처음에 시작할 때 타입을 모르겠으면 any로 지정해준 후
+// 진행하면서 구체적인 타입을 지정해주는 방법이 좋다
+let todoItems: { id: number, title: string, done: boolean }[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): { id: number, title: string, done: boolean }[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,40 +13,61 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(
+  todo: { id: number, title: string, done: boolean }
+  ): void { // return이 없는 함수에는 void로 지정해줌
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(
+  index: number, 
+  todo: { id: number, title: string, done: boolean }
+  ):void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): object[] {
   return todoItems.filter(item => item.done);
+  // return todoItems.filter(function(item) {
+  //   if (item.done) {
+  //     return item;
+  //   }
+  // });
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  const item1 = { 
+    id: 4, 
+    title: '아이템1 추가', 
+    done: false 
+  };
+  addTodo(item1);
+  addTodo({ 
+    id: 5, 
+    title: '아이템2 추가', 
+    done: false 
+  });
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
