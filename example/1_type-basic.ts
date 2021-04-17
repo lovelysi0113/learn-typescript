6e56bf73f822a449b3ef0a24bffc7d488acd43ec
Jung SongYi <lovelysi0113@gmail.com>
2021-04-17 16:27:25 +0900

    3-타입스크립트 기초 - 변수와 함수 타입 정의하기

--- example/1_type-basic.ts
+++ example/1_type-basic.ts
@@ -1,27 +1,32 @@
-// 문자열
+// 1. 문자열
+// JS: const str = 'hello';
+// TS: ': ' 다음에 타입을 써주면 된다
 const str: string = 'hello';
 
-// 숫자
+// 2. 숫자
 const num: number = 10;
 
-// 배열
-const arr: Array<string> = [];
-arr.push('hi');
+// 3. 배열 (2가지 선언 방법이 있음)
+const strArr: Array<string> = [];
+strArr.push('hi');
+const strArr2: string[] = [];
+// strArr2.push(10); -> 타입이 다른 데이터를 넣으면 바로 오류가 난다
+let numArr: Array<number> = [1, 2, 3];
+let numArr2: number[] = [1, 2, 3];
 
-// 배열 - 리터럴 적용
-const items: string[] = [];
-// items.push(10);
-
-// 튜플
+// 4. 튜플
+// 하나의 배열에 각각 다른 타입의 데이터를 넣을 수 있다
 const address: [string, number] = ['판교', 40];
+// 음.. 다른 파일에 정의된 이름들과도 겹치면 안되는 것 같다! 빨간줄이 뜨넹..! 우앙 신기!!
+// 다시 하니까 될때도 있네! 뭔차이인거지? 좀더 알아봐야할듯!
+// 확실한건 하나의 파일 안에서는 겹치는 이름이 있으면 안됨..! (당연한 이야기이지만..ㅋㅋ)
 
-// 객체
+// 5. 객체
 const obj: object = {};
 // obj.a = 10;
-
-// 타입 객체
+// 객체에 각 키에 어떤 타입의 데이터가 들어갈 수 있는지 따로 지정해줄 수 있다
 const person: { age: number; name: string } = { age: 100, name: 'Capt' };
 person.age = 101;
 
-// 진위값
+// 6. 진위값(boolean)
 let isLogin: boolean = false;
--- example/3_functions.ts
+++ example/3_functions.ts
@@ -3,14 +3,18 @@ function sum(a, b) {
   return a + b;
 }
 
+// 함수에서 파라미터와 반환값에 타입을 지정해줄 수 있다
+
+// 1. 함수의 파라미터에 타입을 정의하는 방법
 // 타입스크립트 함수 선언 - 함수의 매개 변수
-function add(a: number, b: number) {
+function add1(a: number, b: number) {
   return a + b;
 }
 
+// 2. 함수의 반환값에 타입을 정의하는 방식
 // 타입스크립트 함수 선언 - 함수의 반환 타입
-function add(a: number, b: number): number {
-  return a + b;
+function add2(a: number, b: number): number {
+  return a + b; // 반환값에 타입 지정했는데 함수에 return이 없으면 에러 발생!
 }
 
 // function add(a: number, b: number): string {
@@ -21,10 +25,13 @@ function add(a: number, b: number): number {
 function log(a: string) {
   console.log(a);
 }
-log('a', 10);
+// log('a', 10); -> 함수에 정의된 인자의 갯수보다 더 많이 /더 적게 주게되면 에러 발생!
+// 타입스크립트에서는 엄격하게 검사해준다!
 
 // 함수 옵셔널 파라미터(optional parameter)
+// 파라미터(인자)를 선택적으로 넣을 수 있다
+// 물음표('?') 사용 - 옵셔널 파라미터로 사용하겠다는 의미
 function printText(text: string, type?: string) {
   console.log(text);
 }
-printText('hi');
+printText('hi'); // 두번째 인자는 옵셔널 파라미터이므로 인자가 1개만 있음에도 오류가 나지 않는다!
