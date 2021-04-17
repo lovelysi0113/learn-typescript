6224d7fc62f01e7cd1cfd62c01fdba8e87e173b4
Jung SongYi <lovelysi0113@gmail.com>
2021-04-17 18:45:09 +0900

    7-연산자를 이용한 타입 정의

--- example/7_union-intersection.ts
+++ example/7_union-intersection.ts
@@ -1,19 +1,22 @@
-function logMessage(value: string) {
-  console.log(value);
-}
-function logMessage(value: number) {
-  console.log(value);
-}
+// function logMessage(value: string) {
+//   console.log(value);
+// }
+// function logMessage(value: number) {
+//   console.log(value);
+// }
 function logMessage(value: any) {
   console.log(value);
 }
 
-// # Union 타입 문법 - `any` 보다는 명시적임
-function logMessage(value: string | number) {
+// # Union 타입 문법('|') - `any` 보다는 명시적임
+// 한가지 이상의 타입을 사용하고 싶을 때 사용
+let songyi: string | number | boolean;
+function logMessage1(value: string | number) {
   console.log(value);
 }
 
-function logMessage(value: string | number) {
+// 타입 가드: 특정 타입으로 타입의 범위를 좁혀나가는(필터링 하는) 과정
+function logMessage2(value: string | number) {
   if (typeof value === 'string') {
     value.toLocaleUpperCase();
   }
@@ -23,7 +26,7 @@ function logMessage(value: string | number) {
   throw new TypeError('value must be string or number')
 }
 
-// # Intersection 타입 문법
+// # Intersection 타입 문법('&')
 interface Developer {
   name: string;
   skill: string;
@@ -34,7 +37,19 @@ interface Person {
   age: number;
 }
 
+// Union
 function askSomeone(someone: Developer | Person) {
   someone.name; // O
-  someone.age; // X
+  //someone.age; // X -> 공통속성(교집합)만 접근 가능
 }
+askSomeone({ name: '디벨로퍼', skill: '웹 개발' });
+askSomeone({ name: '캡틴', age: 100 });
+
+// Intersection
+function findSomeone(someone: Developer & Person) {
+  someone.name; // O
+  someone.age; // O -> 모든 속성에 접근 가능(합집합)
+}
+findSomeone({ name: '디벨로퍼', skill: '웹 개발', age: 30 }); // 모든 속성이 다 필요하다
+//findSomeone({ name: '디벨로퍼', skill: '웹 개발' }); -> 오류 발생
+//findSomeone({ name: '캡틴', age: 100 }); -> 오류 발생
